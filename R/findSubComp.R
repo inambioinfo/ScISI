95db5b515d0030ecc3c60f0eafede5328f863351
tchiang@fhcrc.org <tchiang@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-12-15 01:12:47 +0000

    I have added two data files, ISI and ISIsubC. The constructScISI.R file is a script to construct the two data files. The evidenccat.scheme is a MIPS file that details the evidence codes for each protein.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ScISI@15396 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findSubComp.R
+++ R/findSubComp.R
@@ -19,49 +19,51 @@ findSubComp <- function(bg1, bg2, interSectMat, simMat){
     ##This is a way to gain useful information about two related complexes:
     ##The return value is names of the two complexes, their respective
     ##cardinality, and the cardinality of their mutual intersection.
-    if (sum(rownames(bg1) != rownames(bg2)) == 0
-        && sum(colnames(bg1) != colnames(bg2)) == 0) {
-      if (sum(bg1 != bg2) == 0){
-        for (i in 1:nrow(simMat)){
-          for (j in 1:i){
-            if(simMat[i,j] == 1){
-              rec = list()
-              rec$BG1Comp <- rnames[i]
-              rec$BG2Comp <- cnames[j]
-              rec$orderBG1Comp <- sum(bg1[,rnames[i]])
-              rec$orderBG2Comp <- sum(bg2[,cnames[j]])
-              rec$intersect <- interSectMat[rnames[i], cnames[j]]
-            
-              record1[[counter1]] = rec
-              counter1 = counter1+1
-              toBeRm1 = c(toBeRm1, rnames[i])
-            }
-
-            if (simMat[i,j] != 0 && simMat[i,j] != 1){
-              
-              if (simMat[i,j] == ratioO[rnames[i], cnames[j]]
-                  || simMat[i,j] == ratioT[cnames[j], rnames[i]]){
-                
-                rec = list()
-                rec$BG1Comp = rnames[i]
-                rec$BG2Comp = cnames[j]
-                rec$orderBG1Comp = sum(bg1[,rnames[i]])
-                rec$orderBG2Comp = sum(bg2[,cnames[j]])
-                rec$intersectionBG1BG2 = interSectMat[rnames[i], cnames[j]]
-                record3[[counter3]] = rec
-                counter3 = (counter3)+1
-                if(rec$orderBG1Comp > rec$orderBG2Comp){
-                    toBeRm2 = c(toBeRm2, cnames[j])
+    if (nrow(bg1) == nrow(bg2) && ncol(bg1) == ncol(bg2)){
+        if (sum(rownames(bg1) != rownames(bg2)) == 0
+            && sum(colnames(bg1) != colnames(bg2)) == 0) {
+            if (sum(bg1 != bg2) == 0){
+                for (i in 1:nrow(simMat)){
+                    for (j in 1:i){
+                        if(simMat[i,j] == 1){
+                            rec = list()
+                            rec$BG1Comp <- rnames[i]
+                            rec$BG2Comp <- cnames[j]
+                            rec$orderBG1Comp <- sum(bg1[,rnames[i]])
+                            rec$orderBG2Comp <- sum(bg2[,cnames[j]])
+                            rec$intersect <- interSectMat[rnames[i], cnames[j]]
+                            
+                            record1[[counter1]] = rec
+                            counter1 = counter1+1
+                            toBeRm1 = c(toBeRm1, rnames[i])
+                        }
+                        
+                        if (simMat[i,j] != 0 && simMat[i,j] != 1){
+                            
+                            if (simMat[i,j] == ratioO[rnames[i], cnames[j]]
+                                || simMat[i,j] == ratioT[cnames[j], rnames[i]]){
+                                
+                                rec = list()
+                                rec$BG1Comp = rnames[i]
+                                rec$BG2Comp = cnames[j]
+                                rec$orderBG1Comp = sum(bg1[,rnames[i]])
+                                rec$orderBG2Comp = sum(bg2[,cnames[j]])
+                                rec$intersectionBG1BG2 = interSectMat[rnames[i], cnames[j]]
+                                record3[[counter3]] = rec
+                                counter3 = (counter3)+1
+                                if(rec$orderBG1Comp > rec$orderBG2Comp){
+                                    toBeRm2 = c(toBeRm2, cnames[j])
+                                }
+                                if(rec$orderBG1Comp < rec$orderBG2Comp){
+                                    toBeRm2 = c(toBeRm2, rnames[i])
+                                }
+                                
+                            }
+                        }
+                    }
                 }
-                if(rec$orderBG1Comp < rec$orderBG2Comp){
-                    toBeRm2 = c(toBeRm2, rnames[i])
-                }
-                
-              }
             }
-          }
         }
-      }
     }
 
     else{
---
+++ inst/Scripts/constructScISI.R
@@ -0,0 +1,99 @@
+##This file gives the exact commands for the construction of the ScISI.
+
+library(ScISI)
+
+go = getGOInfo(wantDefault = TRUE, toGrep = NULL, parseType = NULL,
+               eCode = NULL, wantAllComplexes = TRUE)
+goM = createGOMatrix(go)
+go2go = runCompareComplex(goM, goM, byWhich = "ROW")
+rmFromGo = c(go2go$toBeRm, go2go$toBeRmSubC)
+
+mips = getMipsInfo(wantDefault = TRUE, toGrep = NULL, parseType = NULL,
+               eCode = NULL, wantAllComplexes = FALSE)
+mipsM = createMipsMatrix(mips)
+mips2mips = runCompareComplex(mipsM, mipsM, byWhich= "ROW")
+rmFromMips = c(mips2mips$toBeRm, mips2mips$toBeRmSubC)
+
+mips2go = runCompareComplex(mipsM, goM, byWhich= "ROW")
+rmFromMipsGo = c(mips2go$toBeRm, mips2go$toBeRmSubC)
+
+mergeMipsGo = mergeBGMat(mipsM, goM, toBeRm = unique(c(rmFromGo, rmFromMips,
+                                                       rmFromMipsGo)))
+
+gavin = getAPMSData("Gavin")
+gavin2gavin = runCompareComplex(gavin, gavin, byWhich= "ROW")
+rmFromGavin = c(gavin2gavin$toBeRm, gavin2gavin$toBeRmSubC)
+
+gavin2mergeMG = runCompareComplex(gavin, mergeMipsGo, byWhich="ROW")
+rmFromMGG = c(gavin2mergeMG$toBeRm, gavin2mergeMG$toBeRmSubC)
+
+mergeMGG = mergeBGMat(gavin, mergeMipsGo, toBeRm = unique(c(rmFromGavin,
+                                                             rmFromMGG)))
+
+ho = getAPMSData("Ho")
+ho2ho = runCompareComplex(ho, ho, byWhich = "ROW")
+rmFromHo = c(ho2ho$toBeRm, ho2ho$toBeRmSubC)
+
+ho2mergeMGG = runCompareComplex(ho, mergeMGG, byWhich= "ROW")
+rmFromMGGH = c(ho2mergeMGG$toBeRm, ho2mergeMGG$toBeRmSubC)
+
+mergeMGGH = mergeBGMat(ho, mergeMGG, toBeRm = unique(c(rmFromHo, rmFromMGGH)))
+
+krogan = getAPMSData("Krogan")
+krogan2krogan = runCompareComplex(krogan, krogan, byWhich = "ROW")
+rmFromKrogan = c(krogan2krogan$toBeRm, krogan2krogan$toBeRmSubC)
+
+krogan2mergeMGGH = runCompareComplex(krogan, mergeMGGH, byWhich = "ROW")
+rmFromMGGHK = c(krogan2mergeMGGH$toBeRm, krogan2mergeMGGH$toBeRmSubC)
+
+ScISI = mergeBGMat(krogan, mergeMGGH, toBeRm = unique(c(rmFromKrogan,
+                                                        rmFromMGGHK)))
+
+##If the sub-complexes are to be included in the interactome
+
+go = getGOInfo(wantDefault = TRUE, toGrep = NULL, parseType = NULL,
+               eCode = NULL, wantAllComplexes = TRUE)
+goM = createGOMatrix(go)
+go2go = runCompareComplex(goM, goM, byWhich = "ROW")
+rmFromGoEq = c(go2go$toBeRm)
+
+mips = getMipsInfo(wantDefault = TRUE, toGrep = NULL, parseType = NULL,
+               eCode = NULL, wantAllComplexes = TRUE)
+mipsM = createMipsMatrix(mips)
+mips2mips = runCompareComplex(mipsM, mipsM, byWhich= "ROW")
+rmFromMips = c(mips2mips$toBeRm)
+
+mips2go = runCompareComplex(mipsM, goM, byWhich= "ROW")
+rmFromMipsGo = c(mips2go$toBeRm)
+
+mergeMipsGo = mergeBGMat(mipsM, goM, toBeRm = unique(c(rmFromGo, rmFromMips,
+                                                       rmFromMipsGo)))
+
+gavin = getAPMSData("Gavin")
+gavin2gavin = runCompareComplex(gavin, gavin, byWhich= "ROW")
+rmFromGavin = c(gavin2gavin$toBeRm)
+
+gavin2mergeMG = runCompareComplex(gavin, mergeMipsGo, byWhich="ROW")
+rmFromMGG = c(gavin2mergeMG$toBeRm)
+
+mergeMGG = mergeBGMat(gavin, mergeMipsGo, toBeRm = unique(c(rmFromGavin,
+                                                             rmFromMGG)))
+
+ho = getAPMSData("Ho")
+ho2ho = runCompareComplex(ho, ho, byWhich = "ROW")
+rmFromHo = c(ho2ho$toBeRm)
+
+ho2mergeMGG = runCompareComplex(ho, mergeMGG, byWhich= "ROW")
+rmFromMGGH = c(ho2mergeMGG$toBeRm)
+
+mergeMGGH = mergeBGMat(ho, mergeMGG, toBeRm = unique(c(rmFromHo, rmFromMGGH)))
+
+krogan = getAPMSData("Krogan")
+krogan2krogan = runCompareComplex(krogan, krogan, byWhich = "ROW")
+rmFromKrogan = c(krogan2krogan$toBeRm)
+
+krogan2mergeMGGH = runCompareComplex(krogan, mergeMGGH, byWhich = "ROW")
+rmFromMGGHK = c(krogan2mergeMGGH$toBeRm)
+
+ScISIsubC = mergeBGMat(krogan, mergeMGGH, toBeRm = unique(c(rmFromKrogan,
+                                                        rmFromMGGHK)))
