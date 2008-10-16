81041fad3dde3bc743c21d6dcbd5e2c40069b310
p.aboyoun <p.aboyoun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-10-16 19:55:23 +0000

    Switched to using matrices.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ScISI@34596 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Scripts/getMipsY2HInfo.R
+++ inst/Scripts/getMipsY2HInfo.R
@@ -2,10 +2,8 @@ getMipsY2HInfo <- function(wantDefault = TRUE, toGrep = NULL,
                            parseType = NULL, eCode = NULL){
 
     fileToRead <- gzfile(system.file("extdata", "PPI_141105.tab.gz", package = "ScISI"), open = "rb")
-    dataTable <- do.call("rbind.data.frame", strsplit(gsub("\\|$", "||", scan(fileToRead, what = "")), split = "|", fixed = TRUE))
+    dataMat <- do.call("rbind", strsplit(gsub("\\|$", "||", scan(fileToRead, what = "")), split = "|", fixed = TRUE))
     close(fileToRead)
-
-    dataMat = as.matrix(dataTable)
 
     desc = dataMat[,5]
 
